6cd487d9e2fa0210dc4b8556981397ed24dd1c1c
JJB <surpatience@gmail.com>
2025-01-12 15:21:07 +0900

    initialize project

---
+++ src/main/java/com/example/publicHousing/PublicHousingApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicHousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicHousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicHousingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/publicHousing/PublicHousingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicHousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
